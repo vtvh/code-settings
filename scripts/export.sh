d1bff7a6393c689c67a95142a9f4353870c6fba6
Hai Vu <44681077+andreehai1011@users.noreply.github.com>
2020-01-29 07:28:33 +0200

    first commit

---
+++ scripts/export.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+cp editing/*.json ~/AppData/Roaming/Code/User/
---
+++ scripts/import.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+cp ~/AppData/Roaming/Code/User/*.json current/
+cp -r ~/AppData/Roaming/Code/User/snippets current/
